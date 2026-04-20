05010f00f5903d2c322738df49f81220e42bd067
Too-Zestyy <violet.steers.dev@gmail.com>
2026-04-20 23:51:18 +0100

    feat: add simple interaction with postgres DB

--- lib/DancerApp.pm
+++ lib/DancerApp.pm
@@ -1,5 +1,6 @@
 package DancerApp;
 use Dancer2;
+use Dancer2::Plugin::Database;
 
 our $VERSION = '0.1';
 
@@ -7,10 +8,15 @@ get '/' => sub {
     template 'index' => { 'title' => 'DancerApp' };
 };
 
-get '/hello-world' => sub {
+get '/:id' => sub {
     content_type("application/json");
+
+    # my $sth = database->prepare(
+    #     'SELECT * FROM notes WHERE id = ?',
+    # );
+    # $sth->execute(params->{id});
     
-    return to_json({'message' => 'Hello World!!'});
+    return to_json(database->quick_select('Notes', { Id => params->{id} }));
 };
 
 true;
