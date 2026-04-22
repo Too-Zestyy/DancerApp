a79e005501604d40363d320e6dc1b70ea2a6b40d
Too-Zestyy <violet.steers.dev@gmail.com>
2026-04-23 00:49:21 +0100

    [WIP] feat: PoC for simple form entry and use

--- lib/DancerApp.pm
+++ lib/DancerApp.pm
@@ -2,10 +2,18 @@ package DancerApp;
 use Dancer2;
 use Dancer2::Plugin::Database;
 
+use strict;
+use warnings;
+
 our $VERSION = '0.1';
 
 get '/' => sub {
     template 'index' => { 'title' => 'DancerApp' };
+};
+
+get '/search' => sub {
+    my $query = query_parameters->get('note-name');
+    template 'note-search' => {note_query => $query};
 };
 
 get '/:id' => sub {
